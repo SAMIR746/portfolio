7f076a0d8b87047f2af4fa65b8949784c93777f7
samir746 <87543357+SAMIR746@users.noreply.github.com>
2025-12-23 19:11:28 +0600

    Add export statement to Hero component
    Signed-off-by: samir746 <87543357+SAMIR746@users.noreply.github.com>

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,1 +1,2 @@
 // Hero
+export default function Hero({ profile }) { return ( <section className="hero"> <img src={profile.image} alt="Profile" /> <h1>{profile.name}</h1> <h4>{profile.title}</h4> </section> ); }
