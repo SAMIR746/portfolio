9db51c51022229ac6c0f831e2b207093d98eb20e
samir746 <87543357+SAMIR746@users.noreply.github.com>
2025-12-23 19:02:11 +0600

    Add data routes for profile retrieval and update
    Signed-off-by: samir746 <87543357+SAMIR746@users.noreply.github.com>

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,1 +1,20 @@
 // Data routes
+import express from "express";
+import Profile from "../models/Profile.js";
+import auth from "../middleware/auth.js";
+
+const router = express.Router();
+
+// GET profile data
+router.get("/", async (req, res) => {
+  const data = await Profile.findOne();
+  res.json(data);
+});
+
+// POST update profile (protected)
+router.post("/update", auth, async (req, res) => {
+  const updated = await Profile.findOneAndUpdate({}, req.body, { new: true, upsert: true });
+  res.json(updated);
+});
+
+export default router;
