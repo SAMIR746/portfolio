7a53cd61664c054b7bd18ba7bb6d6a6fac68eaab
samir746 <87543357+SAMIR746@users.noreply.github.com>
2025-12-23 18:59:42 +0600

    Update User.js
    Signed-off-by: samir746 <87543357+SAMIR746@users.noreply.github.com>

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,9 @@
 // User model
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true } // hashed later if needed
+});
+
+export default mongoose.model("User", userSchema);
