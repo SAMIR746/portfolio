5ca099f138cd99cfb1d7418e05e777a6102b0fcd
samir746 <87543357+SAMIR746@users.noreply.github.com>
2025-12-23 19:00:06 +0600

    Add Profile model with schema definition
    Signed-off-by: samir746 <87543357+SAMIR746@users.noreply.github.com>

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,1 +1,25 @@
 // Profile model
+import mongoose from "mongoose";
+
+const profileSchema = new mongoose.Schema({
+  name: String,
+  title: String,
+  image: String,
+  about: String,
+  education: [
+    {
+      degree: String,
+      institute: String,
+      result: String,
+      certificates: [String]
+    }
+  ],
+  projects: [
+    {
+      title: String,
+      desc: String
+    }
+  ]
+});
+
+export default mongoose.model("Profile", profileSchema);
