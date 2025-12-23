2c3c9c34be1c838c9ffe53277ee8fda39f764fcb
samir746 <87543357+SAMIR746@users.noreply.github.com>
2025-12-23 19:12:30 +0600

    Add export statement for Projects component
    Signed-off-by: samir746 <87543357+SAMIR746@users.noreply.github.com>

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,1 +1,2 @@
 // Projects
+export default function Projects({ projects }) { return ( <section className="container mt-5"> <h2>Projects</h2> <div className="row"> {projects.map(p => ( <div key={p.id} className="col-md-4"> <div className="card project-card p-3"> <h5>{p.title}</h5> <p>{p.desc}</p> </div> </div> ))} </div> </section> ); }
