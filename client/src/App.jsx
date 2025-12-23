5a2bc84fb08d2b29f090b6fa664c37b9525e95d5
samir746 <87543357+SAMIR746@users.noreply.github.com>
2025-12-23 18:55:53 +0600

    Update App.jsx
    Signed-off-by: samir746 <87543357+SAMIR746@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,31 @@
 // App component
+import { useEffect, useState } from "react";
+import { fetchData } from "./api";
+
+import SocialBar from "./components/SocialBar";
+import Hero from "./components/Hero";
+import Education from "./components/Education";
+import Projects from "./components/Projects";
+import About from "./components/About";
+
+import ThemeProvider from "./context/ThemeContext";
+
+export default function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchData().then(setData);
+  }, []);
+
+  if (!data) return <h2 className="text-center mt-5">Loading...</h2>;
+
+  return (
+    <ThemeProvider>
+      <SocialBar />
+      <Hero profile={data} />
+      <Education education={data.education} />
+      <Projects projects={data.projects.slice(0, 5)} />
+      <About about={data.about} />
+    </ThemeProvider>
+  );
+}
